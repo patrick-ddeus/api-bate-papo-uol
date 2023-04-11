f4dc640bcf1bb682f9291e68d208f7d5513adc18
PatKatXD <100157955+PatKat20@users.noreply.github.com>
2023-04-11 11:36:29 -0300

    create env variables and install mongodb

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import dotenv from "dotenv"
 
 const app = express();
 
+dotenv.config()
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.listen(5000, () => console.log(`
-    Servidor iniciado na porta 5000
+app.listen(process.env.PORT, () => console.log(`
+    Servidor iniciadoe em: http://${process.env.HOST}:${process.env.PORT}
 `));
